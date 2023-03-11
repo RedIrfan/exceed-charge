08931c1838da603e7ff01cceddb14e87189cec66
RedIrfan <iniirfanfauzi@gmail.com>
2023-03-12 01:18:54 +0700

    added level to deck

--- Godot/Objects/Cards/DeckData.gd
+++ Godot/Objects/Cards/DeckData.gd
@@ -2,21 +2,19 @@ extends Resource
 class_name DeckData
 
 @export var body : Character
-@export var CHARGE : int = 5
+@export var CHARGE : int = 6
 @export var DECK_MAX_AMOUNT : int = 5
 @export var deck_list : Array[CardData] = []
 
-var limit_max_charge : int = CHARGE
-var charge : int = 0
+var level : int = 0
+var charge : Array[CardData.SUITS] = []
 
 
-func _init(new_body:Character=null, new_charge : int=5, new_deck_max_amount : int = 5, new_deck_list:Array[CardData]=[]):
+func _init(new_body:Character=null, new_charge : int=10, new_deck_max_amount : int = 5, new_deck_list:Array[CardData]=[]):
 	body = new_body
 	CHARGE = new_charge
 	DECK_MAX_AMOUNT = new_deck_max_amount
 	deck_list = new_deck_list
-	
-	limit_max_charge = CHARGE
 
 
 func add_card(card:CardData) -> bool:
@@ -36,18 +34,29 @@ func remove_card(card_index:int) -> bool:
 
 func use_card(card_index:int) -> bool:
 	if card_index < deck_list.size():
-		if charge < limit_max_charge:
+		if charge.size() < get_maximum_charge():
 			var card = deck_list[card_index]
 			card.process_card(body)
 			
-			charge += card.value
-			if charge >= limit_max_charge:
+			for index in range(0, card.value):
+				charge.append(card.suit)
+			if charge.size() >= get_maximum_charge():
 				exceed_charge()
 			remove_card(card_index)
 	return false
 
 
 func exceed_charge():
-	limit_max_charge -= 1
-	if limit_max_charge > 0:
-		charge = 0
+	var exceed_types : Array = [0,0,0,0]
+	for charge_type in charge:
+		exceed_types[charge_type] += 1
+	
+	var exceed_type = exceed_types.find(exceed_types.max())
+	
+	level += 1
+	if get_maximum_charge() > 0:
+		charge.clear()
+
+
+func get_maximum_charge() -> int:
+	return CHARGE - level
