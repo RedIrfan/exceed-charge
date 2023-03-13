ddf957ed101d61434e72794185fe5eecdba6b254
RedIrfan <iniirfanfauzi@gmail.com>
2023-03-13 21:15:07 +0700

    added hurt state by heavy damage

--- Godot/Objects/Characters/States/StateCharacterHurt.gd
+++ Godot/Objects/Characters/States/StateCharacterHurt.gd
@@ -6,14 +6,23 @@ class_name StateCharacterHurt
 @export var immovable : bool = false
 
 @export_group("Light")
-@export var knockback_speed_light : int = 2
-@export var knockback_duration_light : float = 0.4167
+@export var knockback_speed_light : int = 3
+@export var knockback_duration_light : float = 0.125
+
+@export_subgroup("Animation")
+@export var	animation_duration_light : float = 0.4167
 
 @export_group("Heavy")
 @export var knockback_speed_heavy : int = 5
 @export var knockback_duration_heavy : float = 0.5
+@export var wakeup_duration  : float = 0.5
+
+@export_subgroup("Animation")
+@export var animation_duration_heavy : float = 1.0
+@export var animation_transition_to_idle_duration : float = 0.25
 
 var hurt_data : Hurtdata 
+var hurt_state_index : int = 0
 
 
 func enter_condition(_body, _fsm, msg=[]) -> bool:
@@ -50,6 +59,7 @@ func enter(_msg=[]):
 
 
 func exit():
+	hurt_state_index = 0
 	body.disconnect_from_animation_timer(_on_animation_timeout)
 	reset_direction()
 	reset_speed()
@@ -67,11 +77,22 @@ func _process_damage():
 func _get_knockback() -> Array:
 	match hurt_data.damage_type:
 		Global.DAMAGES.LIGHT:
-			return [knockback_speed_light, knockback_duration_light]
+			return [knockback_speed_light, knockback_duration_light, animation_duration_light-knockback_duration_light]
 		Global.DAMAGES.HEAVY:
-			return [knockback_speed_heavy, knockback_duration_heavy]
+			return [knockback_speed_heavy, knockback_duration_heavy, animation_duration_heavy-knockback_duration_heavy]
 	return [0,0]
 
 
 func _on_animation_timeout():
-	fsm.enter_state("Idle")
+	if hurt_state_index == 0:
+		hurt_state_index = 1
+		reset_direction()
+		body.start_animation_timer(_get_knockback()[2])
+	elif hurt_data.damage_type == Global.DAMAGES.HEAVY and hurt_state_index == 1:
+		hurt_state_index = 2
+		body.play_animation("HurtWakeup", wakeup_duration)
+	elif hurt_data.damage_type == Global.DAMAGES.HEAVY and hurt_state_index == 2:
+		hurt_state_index = 3
+		body.play_animation("Idle", animation_transition_to_idle_duration)
+	else:
+		fsm.enter_state("Idle")
