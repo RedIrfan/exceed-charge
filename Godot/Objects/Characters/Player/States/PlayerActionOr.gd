3fd75738e2ecdeefa5a02ee3f60fcabcc8f58b68
RedIrfan <iniirfanfauzi@gmail.com>
2023-04-19 02:25:57 +0700

    actually fixed bug removed passive card even though the activation is because of the exceed charge

--- Godot/Objects/Characters/Player/States/PlayerActionOr.gd
+++ Godot/Objects/Characters/Player/States/PlayerActionOr.gd
@@ -14,10 +14,9 @@ extends ActionOr
 
 func get_chosen(body : Character) -> ActionData:
 	var chosen : int = 0
-	if condition_exceed_suit != CardData.SUITS.NONE:
-		if body.get_exceed_charge_suit() == condition_exceed_suit:
-			chosen = condition_exceed_action_target
-	if condition_passive_card != null:
+	if condition_exceed_suit != CardData.SUITS.NONE and body.get_exceed_charge_suit() == condition_exceed_suit:
+		chosen = condition_exceed_action_target
+	elif condition_passive_card != null:
 		if body.get_total_passive_card(condition_passive_card.suit, condition_passive_card.value) > 0:
 			body.remove_passive_cards(condition_passive_card.suit, condition_passive_card.value, 1)
 			chosen = condition_passive_action_target
