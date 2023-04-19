d58947da10ee45376eb7f18acdbe5e7b4ff1de83
RedIrfan <iniirfanfauzi@gmail.com>
2023-04-20 04:32:29 +0700

    added four of diamonds card effect can guard crush by pressing forward(relative to mouse) and heavy attack

--- Godot/Objects/Characters/Player/States/SecondaryAttackMaster.gd
+++ Godot/Objects/Characters/Player/States/SecondaryAttackMaster.gd
@@ -1,8 +1,15 @@
 extends StatePlayer
 
 
-func enter(_msg=[]):
-	fsm.enter_state("SlashHeavy")
+func enter(msg=[]):
+	var slash : bool = true
+	if msg.size() > 0:
+		if body.get_total_passive_card(CardData.SUITS.DIAMOND, CardData.VALUES.FOUR) and msg[0] == "Forward":
+			fsm.enter_state("GuardCrush")
+			slash = false
+	
+	if slash:
+		fsm.enter_state("SlashHeavy")
 
 
 func exit():
