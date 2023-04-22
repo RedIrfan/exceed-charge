5c20678b8741cdf17c884b3a5bede3a73c830b5c
RedIrfan <iniirfanfauzi@gmail.com>
2023-04-23 02:35:31 +0700

    fix bug player not playing defended sfx while having a shield

--- Godot/Objects/Characters/Player/States/Hurt.gd
+++ Godot/Objects/Characters/Player/States/Hurt.gd
@@ -4,6 +4,7 @@ extends StateCharacterHurt
 func enter_condition(_body, _fsm, params=[]):
 	body = _body
 	if body.get_total_passive_card(CardData.SUITS.PENTAGON, CardData.VALUES.THREE) > 0:
+		Global.play_sound(SOUND_DEFENDED, body.global_position)
 		hurt_data = body.hurt_data
 		hurt_data.damage = hurt_data.damage * 0.5
 		body.hurt_data = null
