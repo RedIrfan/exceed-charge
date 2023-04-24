75241f2b979cb34ae366144f4fbf6b4ce06e1215
RedIrfan <iniirfanfauzi@gmail.com>
2023-04-24 22:12:04 +0700

    added player shield breaking mechanic player breaks a shield if an attack is a forced damage (a guard crush)

--- Godot/Objects/Characters/Player/States/Hurt.gd
+++ Godot/Objects/Characters/Player/States/Hurt.gd
@@ -3,15 +3,21 @@ extends StateCharacterHurt
 
 func enter_condition(_body, _fsm, params=[]):
 	body = _body
-	if body.get_total_passive_card(CardData.SUITS.PENTAGON, CardData.VALUES.THREE) > 0:
+	if body.get_total_passive_card(CardData.SUITS.PENTAGON, CardData.VALUES.THREE) > 0 and body.hurt_data.force_damage == false:
 		Global.play_sound(SOUND_DEFENDED, body.global_position)
-		hurt_data = body.hurt_data
-		hurt_data.damage = hurt_data.damage * 0.5
+		process_shield(body)
+		
 		body.hurt_data = null
 		_process_damage()
-		
-		body.remove_passive_cards(CardData.SUITS.PENTAGON, CardData.VALUES.THREE, 1)
 	else:
+		if body.get_total_passive_card(CardData.SUITS.PENTAGON, CardData.VALUES.THREE) > 0:
+			process_shield(body)
 		return super.enter_condition(_body, _fsm, params)
 	
 	return false
+
+
+func process_shield(_body):
+	hurt_data = _body.hurt_data
+	hurt_data.damage = hurt_data.damage * 0.5
+	body.remove_passive_cards(CardData.SUITS.PENTAGON, CardData.VALUES.THREE, 1)
