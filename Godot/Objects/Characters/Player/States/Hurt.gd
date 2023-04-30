773d829eb942e78533465d02e48a996b28654695
RedIrfan <iniirfanfauzi@gmail.com>
2023-04-30 12:20:42 +0700

    change defense shield to deuce of pentagon

--- Godot/Objects/Characters/Player/States/Hurt.gd
+++ Godot/Objects/Characters/Player/States/Hurt.gd
@@ -3,14 +3,14 @@ extends StateCharacterHurt
 
 func enter_condition(_body, _fsm, params=[]):
 	body = _body
-	if body.get_total_passive_card(CardData.SUITS.PENTAGON, CardData.VALUES.THREE) > 0 and body.hurt_data.force_damage == false:
+	if body.get_total_passive_card(CardData.SUITS.PENTAGON, CardData.VALUES.DEUCE) > 0 and body.hurt_data.force_damage == false:
 		Global.play_sound(SOUND_DEFENDED, body.global_position)
 		process_shield(body)
 		
 		body.hurt_data = null
 		_process_damage()
 	else:
-		if body.get_total_passive_card(CardData.SUITS.PENTAGON, CardData.VALUES.THREE) > 0:
+		if body.get_total_passive_card(CardData.SUITS.PENTAGON, CardData.VALUES.DEUCE) > 0:
 			process_shield(body)
 		return super.enter_condition(_body, _fsm, params)
 	
@@ -20,4 +20,4 @@ func enter_condition(_body, _fsm, params=[]):
 func process_shield(_body):
 	hurt_data = _body.hurt_data
 	hurt_data.damage = hurt_data.damage * 0.5
-	body.remove_passive_cards(CardData.SUITS.PENTAGON, CardData.VALUES.THREE, 1)
+	body.remove_passive_cards(CardData.SUITS.PENTAGON, CardData.VALUES.DEUCE, 1)
